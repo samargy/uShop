fdd5e00af4ad92adfdd40f762624512f60d7394f
Sam Argy <samargy@Sams-MacBook-Pro.local>
2019-05-06 23:07:13 +1000

    trying to bugfix sign in >:(

--- public/js/main.js
+++ public/js/main.js
@@ -5,22 +5,44 @@ let index = 3;
 
 addButton.style.cursor = 'pointer'
 addButton.onclick = function() {
+    
+   
     createDropdown();
+
+    //Only allowing 3 different categories
+    if(index == 4) {
+        addButton.parentNode.removeChild(addButton)
+    }
+    //Fixup with bootstrap grid system
+    if(index == 3) {
+        let reEnterPassword = document.getElementById("passwordGroup2")
+        reEnterPassword.classList.remove('offset-md-4');
+    }
     index++;
+    let categoryCount = document.getElementById('categoryCount')
+    categoryCount.value = Number(categoryCount.value) + 1;
 }
 
 
 function createDropdown() {
 
     //Getting the row to put the item in.
-    let row = document.getElementById("row3");
+    let row = document.getElementById("row"+String(index));
 
+    //Getting row below
+    let nextRow = document.getElementById("row"+String(index+1));
+
+    //Getting the add button to move to a row below
+    let divRemove = document.getElementById("addButton")
+    nextRow.insertBefore(divRemove, document.getElementById('passwordGroup'+String(index-1)));
+
+    
     //Making the div
     let containerDiv = document.createElement("div");
 
     //Making the label
     let label = document.createElement("label");
-    let labelText = document.createTextNode("Shop Category");
+    let labelText = document.createTextNode("Shop Category "+String(index-1));
     label.appendChild(labelText);
     label.htmlFor = "inputCategory" + String(index-1);
     containerDiv.appendChild(label);
@@ -34,9 +56,21 @@ function createDropdown() {
         option.value = optionsArray[i];
         selBox.appendChild(option)
     }
+
+    containerDiv.classList.add('form-group')
+    containerDiv.classList.add('col-lg-4')
+    containerDiv.style.width = 85%
+    selBox.classList.add('form-control')
+    selBox.classList.add('short-box')
+    selBox.style.width = "85%"
+    selBox.name = "category" + String(index-1);
+    //Putting select box in the div
     containerDiv.appendChild(selBox);
 
+
+    //Getting the element to put the div before
+    let boxOnRight = document.getElementById('passwordGroup'+String(index-2));
     //Putting everything in the row
-    row.appendChild(containerDiv)
+    row.insertBefore(containerDiv, boxOnRight)
 
 }
